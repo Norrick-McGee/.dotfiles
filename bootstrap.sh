3b0c1415ce6ee8fbbca02704f7a5bf9971ca40f6
Norrick McGee <Norrick.McGee@gmail.com>
2023-05-20 18:12:01 -0500

    base fish config

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
 # Install neovim
-sudo pacman -Syu --noconfirm neovim
+sudo pacman -S --noconfirm neovim
 
 # Create neovim config file
 mkdir -p ~/.config/nvim
 touch ~/.config/nvim/init.lua
 
 # Install tmux
-sudo pacman -Syu --noconfirm tmux
+sudo pacman -S --noconfirm tmux
 
 # Create tmux config file
 touch ~/.config/tmux/tmux.conf
 echo "set -g mouse on" >> ~/.config/tmux/tmux.conf
 
 # Install fish
-sudo pacman -Syu --noconfirm fish
+sudo pacman -S --noconfirm fish
 
 # Create fish config file
 mkdir -p ~/.config/fish
+cp fish/config.fish ~/.config/fish/
 touch ~/.config/fish/config.fish
 
 
