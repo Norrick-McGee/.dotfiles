bca56e2c3ae35f5d68cb28f771829f98d70336c0
Norrick McGee <Norrick.McGee@gmail.com>
2023-05-20 18:44:48 -0500

    added -f flag to symlink creation

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,15 +4,15 @@
 sudo pacman -S --noconfirm neovim tmux fish
 
 # Symlink neovim config directory  
-ln -s ~/.dotfiles/nvim/ ~/.config/
+ln -sf ~/.dotfiles/nvim/ ~/.config/
 
 
 # Create tmux config file
-ln -s ~/.dotfiles/tmux/ ~/.config/
+ln -sf ~/.dotfiles/tmux/ ~/.config/
 # echo "set -g mouse on" >> ~/.config/tmux/tmux.conf
 
 
 # Create fish config file
-ln -s ~/.dotfiles/fish/ ~/.config/
+ln -sf ~/.dotfiles/fish/ ~/.config/
 
 
