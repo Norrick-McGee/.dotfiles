6eb0057333671589a96f3287c5201bae66b2e8af
Norrick McGee <Norrick.McGee@gmail.com>
2023-05-20 19:22:18 -0500

    added starship

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Install needed software 
-sudo pacman -S --noconfirm neovim tmux fish
+sudo pacman -S --noconfirm neovim tmux fish starship
 
 # Symlink .dotfile directories and files to .config 
 ln -sf ~/.dotfiles/nvim/ ~/.config/
 ln -sf ~/.dotfiles/tmux/ ~/.config/
 ln -sf ~/.dotfiles/fish/ ~/.config/
+ln -sf ~/.dotfiles/starship.toml ~/.config/
 
 
