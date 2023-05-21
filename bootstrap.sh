67cb4e590ef5a127cfa4545cebc04b53d4436141
Norrick McGee <Norrick.McGee@gmail.com>
2023-05-20 20:25:29 -0500

    base nvim config

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,10 @@
 # Install needed software 
 sudo pacman -S --noconfirm neovim tmux fish starship
 
+# Download Packer (neovim package manager) 
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
 # Symlink .dotfile directories and files to .config 
 ln -sf ~/.dotfiles/nvim/ ~/.config/
 ln -sf ~/.dotfiles/tmux/ ~/.config/
