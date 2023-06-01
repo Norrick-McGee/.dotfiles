a6b84313172b34580266ed4b1ade9305b19cd089
Norrick McGee <Norrick.McGee@gmail.com>
2023-05-31 20:20:37 -0500

    default ls changed

--- fish/config.fish
+++ fish/config.fish
@@ -92,9 +92,10 @@ end
 
 ## Useful aliases
 # Replace ls with exa
-alias ls='exa -al --color=always --group-directories-first --icons' # preferred listing
+alias ls='exa --color=always --group-directories-first --icons' # preferred listing
 alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
 alias ll='exa -l --color=always --group-directories-first --icons'  # long format
+alias lla='exa -la --color=always --group-directories-first --icons'
 alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
 alias l.='exa -ald --color=always --group-directories-first --icons .*' # show only dotfiles
 alias ip='ip -color'
