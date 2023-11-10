c4027545c70d846381e6057bb0d8260f5c4debdf
Norrick McGee <norrick.mcgee@gmail.com>
2023-11-10 21:00:21 +0000

    change shell to fish automagically

--- bootstrap/ubuntu.sh
+++ bootstrap/ubuntu.sh
@@ -46,5 +46,6 @@ ln -sf $HOME/.dotfiles/fish/ $HOME/.config/
 ln -sf $HOME/.dotfiles/starship.toml $HOME/.config/
 ln -sf $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
 
+sudo cp chsh /etc/pam.d/chsh
 chsh -s $(which fish)
 
